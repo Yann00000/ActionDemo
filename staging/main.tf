ecbb5a5e4136252967bf7b166e8865d831cbec0f
Yann00000 <105195527+Yann00000@users.noreply.github.com>
2022-10-18 15:47:59 +0800

    test

--- staging/main.tf
+++ staging/main.tf
@@ -5,14 +5,29 @@ resource "aws_s3_bucket" "b" {
     Name        = "My bucket"
     Environment = "Dev"
   }
-
-
-
-
-
-
-  
 }
+
+
+terraform {
+  required_version = ">= 1.2.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.34.0"
+    }
+  }
+
+  backend "s3" {
+    encrypt        = true
+    bucket         = "tf_org_statefile"
+    dynamodb_table = "terraform-backend-lock"
+    region         = "us-west-1"
+    key            = "stg/test/terraform.tfstate"
+  }
+
+}
+
 
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
